a3fd5fe1a8d3f084beb5bc7b0a232e837db8ec93
William Bjerke <bjerkewilliam@yahoo.com>
2022-05-05 10:27:10 -0500

    I shouldn't have done this in main, oops, but here is the project boilerplae. It runs but I think intelij has problems identifying it. Probably a cacheing problem on my end.

---
+++ GroupDP2/src/main/java/com/revature/GroupDP2/GroupDp2Application.java
@@ -0,0 +1,13 @@
+package com.revature.GroupDP2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupDp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupDp2Application.class, args);
+	}
+
+}
---
+++ GroupDP2/src/test/java/com/revature/GroupDP2/GroupDp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.GroupDP2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupDp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
