cf8f025b141b5592417095f9538d3d11541aaa4b
Frederick_Cashaw <f_cashaw@yahoo.com>
2022-05-06 10:29:24 -0400

    First Push of Product code!

--- src/main/java/com/revature/GroupDP2/GroupDp2Application.java
+++ src/main/java/com/revature/GroupDP2/GroupDp2Application.java
@@ -1,5 +1,8 @@
 package com.revature.GroupDP2;
 
+import com.revature.GroupDP2.model.Product;
+import com.revature.GroupDP2.util.StorageManager;
+import com.revature.GroupDP2.util.TransactionManager;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -7,12 +10,28 @@ import org.hibernate.cfg.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class GroupDp2Application {
 
 
 	public static void main(String[] args) {
 		SpringApplication.run(GroupDp2Application.class, args);
+
+		StorageManager storageManager = new StorageManager();
+		storageManager.addAnnotatedClass(Product.class);
+
+		Session session = storageManager.initializeDatasource();
+
+		TransactionManager transactionManager = new TransactionManager(session);
+
+		Transaction tx = transactionManager.beginTransaction();
+		Product newProduct = new Product(1L, "Pop", "Micheal Greatest Hits", "9.99", 1L);
+
+		session.save(newProduct);
+		transactionManager.commitTransaction(tx);
+
 
 	}
 
