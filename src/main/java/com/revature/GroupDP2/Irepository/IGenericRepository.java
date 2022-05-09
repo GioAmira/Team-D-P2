8ff182e87cb86b3a5f6f53d7b7ebdff04e5bda56
William Bjerke <bjerkewilliam@yahoo.com>
2022-05-09 14:41:23 -0500

    fixed user, changed generic repository interface to return optional

--- src/main/java/com/revature/GroupDP2/Irepository/IGenericRepository.java
+++ src/main/java/com/revature/GroupDP2/Irepository/IGenericRepository.java
@@ -1,8 +1,10 @@
 package com.revature.GroupDP2.Irepository;
 
+import java.util.Optional;
+
 public interface IGenericRepository<T> {
     void create(T t);
     void update(T t);
-    T getById(int t);
+    Optional<T> getById(int t);
     void delete(T t);
 }
--- src/main/java/com/revature/GroupDP2/Irepository/IUserRepository.java
+++ src/main/java/com/revature/GroupDP2/Irepository/IUserRepository.java
@@ -3,8 +3,9 @@ package com.revature.GroupDP2.Irepository;
 import org.apache.catalina.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IUserRepository extends IGenericRepository<User>{
-    public User getByUsername(String username);
+    public Optional<User> getByUsername(String username);
 
 }
