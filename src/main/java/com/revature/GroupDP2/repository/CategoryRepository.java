c1588460d8405a16b046a835a6a440dce4ff78b7
Laflammex <92762772+GioAmira@users.noreply.github.com>
2022-05-08 23:03:50 -0400

    Added CategoryRepository class and removed some of the .gitkeep files

---
+++ src/main/java/com/revature/GroupDP2/repository/CategoryRepository.java
@@ -0,0 +1,38 @@
+package com.revature.GroupDP2.repository;
+
+import com.google.gson.JsonObject;
+import com.revature.GroupDP2.Irepository.ICategoryRepository;
+import org.json.JSONObject;
+
+public class CategoryRepository implements ICategoryRepository {
+
+    @Override
+    public JSONObject getByCategoryName(JSONObject jsonObject) {
+        return null;
+    }
+
+    @Override
+    public JsonObject getByCategoryName(JsonObject jsonObject) {
+        return null;
+    }
+
+    @Override
+    public void create(Object o) {
+
+    }
+
+    @Override
+    public void update(Object o) {
+
+    }
+
+    @Override
+    public Object getById(int t) {
+        return null;
+    }
+
+    @Override
+    public void delete(Object o) {
+
+    }
+}
