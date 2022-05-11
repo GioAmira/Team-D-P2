2d8f13584d0712df8d02ae598a796fe192991f53
William Bjerke <bjerkewilliam@yahoo.com>
2022-05-10 23:46:49 -0500

    made beans, and bean related comaptability. UserRepository autowires a session via the new sessionstore

--- src/main/java/com/revature/GroupDP2/repository/UserRepository.java
+++ src/main/java/com/revature/GroupDP2/repository/UserRepository.java
@@ -1,37 +1,37 @@
 package com.revature.GroupDP2.repository;
 
 import com.revature.GroupDP2.Irepository.IUserRepository;
+import com.revature.GroupDP2.util.SessionStore;
 import com.revature.GroupDP2.util.TransactionManager;
 import org.apache.catalina.User;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.TypedQuery;
 import java.util.List;
 import java.util.Optional;
-
+@Repository
 public class UserRepository implements IUserRepository {
-    private TransactionManager transactionManager;
-    private Session session;
-    //dependency injection via constructor
-    public UserRepository(TransactionManager transactionManager,Session session) {
-        this.transactionManager = transactionManager;
-        this.session =session;
+    private final Session session;
+    @Autowired
+    public UserRepository(SessionStore sessionstore) {
+        this.session =sessionstore.getSession();
     }
-
 
     @Override
     public void create(User o) {
-        Transaction transaction = transactionManager.beginTransaction();
+        Transaction t=session.beginTransaction();
         session.save(o);
-        transaction.commit();
+        t.commit();
     }
 
     @Override
     public void update(User o) {
-        Transaction transaction = transactionManager.beginTransaction();
+        Transaction t=session.beginTransaction();
         session.update(o);
-        transaction.commit();
+        t.commit();
     }
 
     //maybe we want to return an optional?
@@ -44,9 +44,9 @@ public class UserRepository implements IUserRepository {
 
     @Override
     public void delete(User o) {
-        Transaction transaction = transactionManager.beginTransaction();
+        Transaction t=session.beginTransaction();
         session.delete(o);
-        transaction.commit();
+        t.commit();
     }
 
     @Override
--- src/main/java/com/revature/GroupDP2/util/TransactionManager.java
+++ src/main/java/com/revature/GroupDP2/util/TransactionManager.java
@@ -2,10 +2,11 @@ package com.revature.GroupDP2.util;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 public class TransactionManager {
     private Session session;
-
     public TransactionManager(Session session) {
         this.session = session;
     }
