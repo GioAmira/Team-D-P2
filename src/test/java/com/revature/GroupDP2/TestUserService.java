a4911d5538a5a5a1a43901ce679898b7364281cc
William Bjerke <bjerkewilliam@yahoo.com>
2022-05-13 15:30:54 -0500

    add mockito dependency

--- src/test/java/com/revature/GroupDP2/TestUserService.java
+++ src/test/java/com/revature/GroupDP2/TestUserService.java
@@ -1,8 +1,32 @@
 package com.revature.GroupDP2;
 
+import com.revature.GroupDP2.repository.UserRepository;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 
+import static org.mockito.Mockito.mock;
+
 public class TestUserService {
+    private static UserRepository userRepository;
+    @BeforeClass
+    public static void beforeC(){
+    userRepository=mock(UserRepository.class);
+    }
+    @Before
+    public void before(){
+
+    }
+    @AfterClass
+    public static void afterC(){
+
+    }
+    @After
+    public void after(){
+
+    }
     @Test
     public void testUserRegisterSuccess(){
 
