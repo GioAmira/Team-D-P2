5e4d4992bd2406296cc2b5011b953e0ee4138aae
William Bjerke <bjerkewilliam@yahoo.com>
2022-05-13 15:20:49 -0500

    unit test begin!

--- src/test/java/com/revature/GroupDP2/TestUserService.java
+++ src/test/java/com/revature/GroupDP2/TestUserService.java
@@ -1,2 +1,10 @@
-package com.revature.GroupDP2;public class TestUserService {
+package com.revature.GroupDP2;
+
+import org.junit.jupiter.api.Test;
+
+public class TestUserService {
+    @Test
+    public void testUserRegisterSuccess(){
+
+    }
 }
