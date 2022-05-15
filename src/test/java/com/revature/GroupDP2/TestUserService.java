c7e15502d9108c13944ba8970bbf319c1fd16073
William Bjerke <bjerkewilliam@yahoo.com>
2022-05-14 22:53:57 -0500

    added tests

--- src/test/java/com/revature/GroupDP2/TestUserService.java
+++ src/test/java/com/revature/GroupDP2/TestUserService.java
@@ -4,11 +4,8 @@ import com.revature.GroupDP2.model.User;
 import com.revature.GroupDP2.repository.UserRepository;
 import com.revature.GroupDP2.services.UserService;
 import com.revature.GroupDP2.util.StorageManager;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.parallel.Execution;
 
 import java.util.Optional;
 
@@ -19,33 +16,52 @@ public class TestUserService {
     private static UserRepository userRepository;
     private static UserService userService;
     private static User tu1;
-    /*
-    @BeforeClass
+    private static User tu2;
+
+    @BeforeAll
     public static void beforeC(){
-
+        userRepository=mock(UserRepository.class);
+        userRepository=mock(UserRepository.class);
+        userService = new UserService(userRepository);
     }
-    @Before
+    @BeforeEach
     public void before(){
-
+        tu1=new User("username","password",true,"John","Test",
+                "EMAIL@EMAIL.COM","phone","street","city","state","zip");
+        tu2=new User("wrong","wrong",true,"Mark","Test",
+                "EMAIL@EMAIL.COM","phone","street","city","state","zip");
     }
-    @AfterClass
+    @AfterAll
     public static void afterC(){
 
     }
-    @After
+    @AfterEach
     public void after(){
 
     }
-    */
 
     @Test
     public void testUserRegisterSuccess() throws Exception {
-        userRepository=mock(UserRepository.class);
         when(userRepository.getByUsername("username")).thenReturn(Optional.empty());
-        userService = new UserService(userRepository);
-        tu1=new User("username","password",true,"John","Test",
-                "email","phone","street","city","state","zip");
-        System.out.println("befor e haas run");
         userService.register(tu1);
     }
+    @Test
+    public void testUserRegisterFailAlreadyInsertedUser(){
+        //I took this code from online =-)
+        when(userRepository.getByUsername("username")).thenReturn(Optional.of(tu1));
+        Exception thrown = Assertions.assertThrows(Exception.class, () -> {
+            userService.register(tu1);
+        });
+        Assertions.assertEquals("username already taken!", thrown.getMessage());
+    }
+    @Test
+    public void testUserRegisterFailBadEmail(){
+        tu1.setEmail("BADBADEMAIL.NONSENSE");
+        when(userRepository.getByUsername("username")).thenReturn(Optional.empty());
+        Exception thrown = Assertions.assertThrows(Exception.class, () -> {
+            userService.register(tu1);
+        });
+        Assertions.assertEquals("email invalid!", thrown.getMessage());
+    }
+
 }
