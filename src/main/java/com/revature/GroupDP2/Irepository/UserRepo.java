b99329a9d79e90bbfffe509f01fd7a3f30852336
Laflammex <92762772+GioAmira@users.noreply.github.com>
2022-05-18 02:17:15 -0400

    JWT implementation

--- src/main/java/com/revature/GroupDP2/Irepository/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.revature.GroupDP2.Irepository;
-
-import com.revature.GroupDP2.model.User;
-
-import java.util.Optional;
-
-public interface UserRepo {
-    Optional<User> getByUsername(String username);
-}
